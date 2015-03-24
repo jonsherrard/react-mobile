a991ee41151c19a9625ec22d8332b78b1fdaf703
Jon Sherrard <jon@andthats.it>
2015-03-24 01:52:14 +0000

    flickering

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,13 @@
+// CSS
+require('./app.styl')
+
 var React = require('react');
 var TransitionGroup = require('react/lib/ReactCSSTransitionGroup')
 var Router = require('react-router');
 var { Route, RouteHandler, Link } = Router;
 
-// CSS
-require('./app.styl')
+var iScroll = require('iscroll')
+
 
 // Components
 var Navigation = require('components/navigation')
@@ -13,25 +16,30 @@ const APPNAME = "React Mobile"
 
 class App extends React.Component {
 
+  componentDidMount () {
+    this.iScroll = new iScroll(this.refs.scroller.getDOMNode(), {scrollY: true, mousewheel: true})
+    console.log(this.iScroll)
+  }
+
   render () {
-    var params = this.context.router.getCurrentPath()
-    console.log(params)
     var name = this.context.router.getCurrentPath()
     return (
       <div>
         <Navigation appName={APPNAME} />
-        <div className="inner-app-block">
-          <TransitionGroup
-            component='div'
-            className={`page-transition page-${name}`}
-            transitionName='page-left-right'
-          >
-            <RouteHandler 
-              key={name}
-              appName={APPNAME}
-              {...this.props}
-            />
-          </TransitionGroup>
+        <div className="inner-app-block" ref="scroller">
+          <div className="inner-app-block__scroller">
+            <TransitionGroup
+              component='div'
+              className={`page-transition page-${name}`}
+              transitionName='page-left-right'
+            >
+              <RouteHandler 
+                key={name}
+                appName={APPNAME}
+                {...this.props}
+              />
+            </TransitionGroup>
+          </div>
         </div>
       </div>
     )
--- src/app/screens/home/index.jsx
+++ src/app/screens/home/index.jsx
@@ -1,10 +1,16 @@
 var React = require('react')
 var { Link } = require('react-router')
 
+
+
 class Home extends React.Component {
 
-  constructor () {
-    console.log('Home Screen')
+  _renderList () {
+    var listArray = []
+    for (var i =0; i < 100; i++) {
+      listArray.push(<li key={i} >Test {i} </li>)
+    }
+    return listArray
   }
 
   render () {
@@ -12,6 +18,9 @@ class Home extends React.Component {
       <div className="page">
         <h1>Home</h1>
         <Link to="settings">Settings</Link>
+        <ul>
+          {this._renderList()}
+        </ul>
       </div>
     )
   }
