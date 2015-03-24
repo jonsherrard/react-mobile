e421ae9d2b154b8412be4b295c4367577584679e
Jon Sherrard <jon.sherrard@shortlist.com>
2015-03-24 13:46:58 +0000

    ios7 colors and animations

--- src/app/components/navigation/index.jsx
+++ src/app/components/navigation/index.jsx
@@ -16,20 +16,22 @@ class Navigation extends React.Component {
   }
 
   _backButton () {
-    return (
-      <span
-        onClick={this._goBack.bind(this)}
-        className="navigation-block__back-button"
-      >
-        <svg  x="0px" y="0px" className="navigation-block__svg"
-           width="75.629px" height="75.629px" viewBox="10.148 12.185 75.629 75.629" enable-background="new 10.148 12.185 75.629 75.629"
-          >
-          <circle fill="none" stroke="#ffffff" stroke-width="6" stroke-miterlimit="10" cx="47.963" cy="50" r="34.815"/>
-          <polygon fill="#fff" stroke="#fff" points="69.443,52.222 34.629,52.222 45,62.592 39.074,62.592 26.481,50 39.074,37.407 45,37.407 34.629,47.777 
-            69.443,47.777   "/>
-        </svg>
-      </span>
-    )
+    if (this.context.router.getCurrentPathname() !== '/') {
+      return (
+        <span
+          onClick={this._goBack.bind(this)}
+          className="navigation-block__back-button"
+        >
+          <svg  x="0px" y="0px" className="navigation-block__svg"
+             width="75.629px" height="75.629px" viewBox="10.148 12.185 75.629 75.629" enable-background="new 10.148 12.185 75.629 75.629"
+            >
+            <circle fill="none" stroke="#ffffff" stroke-width="6" stroke-miterlimit="10" cx="47.963" cy="50" r="34.815"/>
+            <polygon fill="#fff" stroke="#fff" points="69.443,52.222 34.629,52.222 45,62.592 39.074,62.592 26.481,50 39.074,37.407 45,37.407 34.629,47.777 
+              69.443,47.777   "/>
+          </svg>
+        </span>
+      )
+    }
   }
 
   render () {
