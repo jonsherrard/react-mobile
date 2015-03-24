744667dba5fdfe9ba7513fc3c0a18c44bad9edb8
Jon Sherrard <jon.sherrard@shortlist.com>
2015-03-24 13:10:14 +0000

    tab navigation

--- src/app/components/tabs/index.jsx
+++ src/app/components/tabs/index.jsx
@@ -3,11 +3,18 @@ require('./tabs.styl')
 
 var React = require('react')
 
+var { Link } = require('react-router')
+
 class Tabs extends React.Component {
   render () {
     return (
       <div className="tabs-block">
-        <span>Tabs</span>
+        <Link to="home" className="tabs-block__button">
+          Home
+        </Link>
+        <Link to="settings" className="tabs-block__button">
+          Settings
+        </Link>
       </div>
     )
   }
